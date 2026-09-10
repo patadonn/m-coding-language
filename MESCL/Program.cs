ce311d4007ac6016733ee9caf0edeb4a35043cc2
PatadonN <matvexreal@gmail.com>
2026-09-10 14:36:01 +0300

    Tokenize Upd

---
+++ MESCL/Program.cs
@@ -0,0 +1,25 @@
+﻿using MESCL;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+var токены = new List<Dictionary<string, (Tokenizator.ТипТокена, string)>>();
+
+токены = Tokenizator.Токенизовать("переменная А = 192.178;\n" +
+    "переменная Б = \"89\";\n" +
+    "переменная Ц,Д = $пропуск#,$0,5#;\n" + 
+    "если переменная (Ц == А) $ \n" +
+    "отладить(\"Верно!\");\n" +
+    "#\n" +
+    "иначе $\n" +
+    "отладить(\"Неверно!\");\n" +
+    "#");
+
+for (int i = 0; i < токены.Count; i++)
+{
+    foreach (var пара in токены[i])  // перебираем словарь
+    {
+        var (тип, значение) = пара.Value;  // распаковываем кортеж
+        Console.WriteLine($"{тип}: '{значение}'");
+    }
+}
